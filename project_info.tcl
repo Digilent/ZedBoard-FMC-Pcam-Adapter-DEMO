2d1db555ba3d387ea89c35b582b15eaef4222081
Vanca Bogdan <bogdan.vanca@digilent.ro>
2019-05-20 16:45:03 +0300

    MOD:FIX: added board part to project_info.tcl and update the system.tcl file

--- project_info.tcl
+++ project_info.tcl
@@ -2,6 +2,7 @@
 proc set_digilent_project_properties {proj_name} {
     set project_obj [get_projects $proj_name]
 	set_property "part" "xc7z020clg484-1" $project_obj
+	set_property "board_part" "em.avnet.com:zed:part0:1.4" $project_obj
 	set_property "default_lib" "xil_defaultlib" $project_obj
 	set_property "simulator_language" "Mixed" $project_obj
 	set_property "target_language" "VHDL" $project_obj
--- src/bd/system.tcl
+++ src/bd/system.tcl
@@ -51,6 +51,7 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 set list_projs [get_projects -quiet]
 if { $list_projs eq "" } {
    create_project project_1 myproj -part xc7z020clg484-1
+   set_property BOARD_PART em.avnet.com:zed:part0:1.4 [current_project]
 }
 
 
